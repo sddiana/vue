55584856329e0758e0002eaa461578e115e773df
sddiana <mosethvatit@gmail.com>
2026-03-03 02:02:04 +0700

    7 practical

--- js/main.js
+++ js/main.js
@@ -2,28 +2,51 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
+       brand: 'Vue Mastery',
        description: "A pair of warm, fuzzy socks",
-       image: "./assets/vmSocks-green-onWhite.jpg",
+       selectedVariant: 0,
        altText: "A pair of socks",
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       inStock: false,
-       onSale: "onSale",
+       onSale: false,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
        variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
        cart: 0,
    },
+
+   computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image () {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale () {
+            if(this.onSale) {
+                return this.brand + ' ' + this.product + ' On sale';
+            } else {
+                return this.brand + ' ' + this.product + ' is not sale';
+            }
+        }
+        
+    },
+
    methods: {
         addToCart() {
             this.cart += 1
@@ -31,9 +54,11 @@ let app = new Vue({
         deleteFromCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
+        
 
     }
 })
