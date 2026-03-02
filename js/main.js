e369b6fe0908d328ff366941a8e046eb764d1a37
sddiana <mosethvatit@gmail.com>
2026-03-02 15:12:03 +0700

    3 practical

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
+       description: "A pair of warm, fuzzy socks",
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
-       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+       inStock: true,
+       inventory: 100,
+       onSale: "onSale"
    }
 })
 
