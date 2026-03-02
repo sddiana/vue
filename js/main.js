a6294a8af142f19e0456c35ac4713dbf959458ab
sddiana <mosethvatit@gmail.com>
2026-03-02 16:26:56 +0700

    6 practical

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@ let app = new Vue({
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       inStock: true,
-       inventory: 100,
+       inStock: false,
        onSale: "onSale",
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
        variants: [
