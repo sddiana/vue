25af990fff7c76fa50a38e012e3e86a147c0ee70
sddiana <mosethvatit@gmail.com>
2026-03-03 03:02:56 +0700

    8 practical

--- js/main.js
+++ js/main.js
@@ -1,66 +1,143 @@
-let app = new Vue({
-   el: '#app',
-   data: {
-       product: "Socks",
-       brand: 'Vue Mastery',
-       description: "A pair of warm, fuzzy socks",
-       selectedVariant: 0,
-       altText: "A pair of socks",
-       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       onSale: false,
-       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-       variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
-            }
-        ],
-       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-       cart: 0,
-   },
+Vue.component('product', {
+   template: `
+   <div class="product">
+       <div class="product-image">
+            <img :src="image" :alt="altText" />
+       </div>
+       <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p>{{ description }}</p>
+            <a :href="link">More products like this</a>
+            <p v-if="inStock">In Stock</p>
+            <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+            <!--<span v-show="onSale">On sale</span>-->
+            <p>{{ sale }}</p>
+            <product-details :details="details"></product-details>
 
-   computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image () {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity;
-        },
-        sale () {
-            if(this.onSale) {
-                return this.brand + ' ' + this.product + ' On sale';
-            } else {
-                return this.brand + ' ' + this.product + ' is not sale';
-            }
-        }
+            <div
+                class="color-box"
+                v-for="(variant, index) in variants"
+                :key="variant.variantId"
+                :style="{ backgroundColor:variant.variantColor }"
+                @mouseover="updateProduct(index)">
+            </div>
+                <ul v-for="size in sizes">
+                    <li>{{ size }}</li>
+                </ul>
+            <p>Shipping: {{ shipping }}</p>
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+            <button v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+            >
+                Add to cart
+            </button>
+            <button v-on:click="deleteFromCart">Delete from cart</button>
+
+       </div>
         
+   </div>
+ `,
+     props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
     },
 
-   methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        deleteFromCart() {
-            this.cart -= 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
+    data() {
+        return {
+                product: "Socks",
+                brand: 'Vue Mastery',
+                description: "A pair of warm, fuzzy socks",
+                selectedVariant: 0,
+                altText: "A pair of socks",
+                link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+                onSale: false,
+                details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+                variants: [
+                        {
+                            variantId: 2234,
+                            variantColor: 'green',
+                            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                            variantQuantity: 10
+                        },
+                        {
+                            variantId: 2235,
+                            variantColor: 'blue',
+                            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                            variantQuantity: 0
+                        }
+                    ],
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+                cart: 0,
         }
-        
+    },
 
+    computed: {
+            title() {
+                return this.brand + ' ' + this.product;
+            },
+            image () {
+                return this.variants[this.selectedVariant].variantImage;
+            },
+            inStock() {
+                return this.variants[this.selectedVariant].variantQuantity;
+            },
+            sale () {
+                if(this.onSale) {
+                    return this.brand + ' ' + this.product + ' On sale';
+                } else {
+                    return this.brand + ' ' + this.product + ' is not sale';
+                }
+            },
+            shipping() {
+                if (this.premium) {
+                    return "Free";
+                } else {
+                    return 2.99
+                }
+}
+
+            
+        },
+
+    methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            deleteFromCart() {
+                this.cart -= 1
+            },
+            updateProduct(index) {
+                this.selectedVariant = index;
+                console.log(index);
+            }
+            
+
+        }
+})
+
+Vue.component('product-details', {
+    template: `
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `,
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
     }
 })
 
+let app = new Vue({
+   el: '#app',
+   data: {
+       premium: true
+   }
 
+})
